3af9283a94bc677fd09f860f8d584604e0d8ea71
therifian <therifianeagle@gmail.com>
2023-06-25 14:43:07 +0200

    feat: 02 2015 part 2

--- AoC2015/src/main/java/com/therifian/AoC2015/Day02.java
+++ AoC2015/src/main/java/com/therifian/AoC2015/Day02.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 
 public class Day02 {
 
-    public static int solve(int l, int w, int h) {
-        int [] arr = {l*w, w*h, h*l};
+    public static int part1(int l, int w, int h) {
+        int[] arr = {l*w, w*h, h*l};
         Arrays.sort(arr);
         return 2*arr[0] + 2*arr[1] + 2*arr[2] + arr[0];
+    }
+
+    public static int part2(int l, int w, int h) {
+        int[] arr = {l, w, h};
+        Arrays.sort(arr);
+        return (2*arr[0] + 2*arr[1]) + (arr[0]*arr[1]*arr[2]);
     }
 }
--- AoC2015/src/test/java/com/therifian/AoC2015/Day02Test.java
+++ AoC2015/src/test/java/com/therifian/AoC2015/Day02Test.java
@@ -8,11 +8,21 @@ public class Day02Test {
 
     @Test
     void test() {
-        assertEquals(58, Day02.solve(2, 3, 4));
+        assertEquals(58, Day02.part1(2, 3, 4));
     }
 
     @Test
     void test2() {
-        assertEquals(43, Day02.solve(1, 1, 10));
+        assertEquals(43, Day02.part1(1, 1, 10));
+    }
+
+    @Test
+    void test3() {
+        assertEquals(34, Day02.part2(2, 3, 4));
+    }
+
+    @Test
+    void test4() {
+        assertEquals(14, Day02.part2(1, 1, 10));
     }
 }
