69c8a45308f1d0445ee9e6184fb9855b3c109b7e
therifian <therifianeagle@gmail.com>
2023-06-25 15:13:04 +0200

    feat: 03 2015 part 2

--- AoC2015/src/main/java/com/therifian/AoC2015/Day03.java
+++ AoC2015/src/main/java/com/therifian/AoC2015/Day03.java
@@ -4,21 +4,41 @@ import java.util.*;
 
 public class Day03 {
 
-    public static int solve(String text) {
+    private static Set<List<Integer>> houses(String input) {
         Set<List<Integer>> houses = new HashSet<>();
 
         int x = 0;
         int y = 0;
         houses.add(List.of(x, y));
 
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == '<') x--;
-            if (text.charAt(i) == '>') x++;
-            if (text.charAt(i) == '^') y++;
-            if (text.charAt(i) == 'v') y--;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '<') x--;
+            if (input.charAt(i) == '>') x++;
+            if (input.charAt(i) == '^') y++;
+            if (input.charAt(i) == 'v') y--;
             houses.add(List.of(x, y));
         }
 
-        return houses.size();
+        return houses;
+    }
+
+    public static int part1(String input) {
+        return houses(input).size();
+    }
+
+    public static int part2(String input) {
+        StringBuilder santaLocations = new StringBuilder();
+        StringBuilder robotLocations = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (i % 2 == 0) santaLocations.append(input.charAt(i));
+            else robotLocations.append(input.charAt(i));
+        }
+
+        Set<List<Integer>> santaHouses = houses(santaLocations.toString());
+        Set<List<Integer>>  robotHouses = houses(robotLocations.toString());
+
+        robotHouses.removeAll(santaHouses);
+        return santaHouses.size() + robotHouses.size();
     }
 }
--- AoC2015/src/test/java/com/therifian/AoC2015/Day03Test.java
+++ AoC2015/src/test/java/com/therifian/AoC2015/Day03Test.java
@@ -8,16 +8,32 @@ public class Day03Test {
 
     @Test
     void test() {
-        assertEquals(2, Day03.solve(">"));
+        assertEquals(2, Day03.part1(">"));
     }
 
     @Test
     void test2() {
-        assertEquals(4, Day03.solve("^>v<"));
+        assertEquals(4, Day03.part1("^>v<"));
     }
 
     @Test
     void test3() {
-        assertEquals(2, Day03.solve("^v^v^v^v^v"));
+        assertEquals(2, Day03.part1("^v^v^v^v^v"));
     }
+
+    @Test
+    void test4() {
+        assertEquals(3, Day03.part2("^v"));
+    }
+
+    @Test
+    void test5() {
+        assertEquals(3, Day03.part2("^>v<"));
+    }
+
+    @Test
+    void test6() {
+        assertEquals(11, Day03.part2("^v^v^v^v^v"));
+    }
+
 }
