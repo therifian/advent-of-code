5a3fb747dc397a46f3de2669023e1c99abb6c464
therifian <therifinaneagle@gmail.com>
2023-08-05 21:08:04 +0200

    feat: 06 2015 part 2

--- AoC2015/src/main/java/com/therifian/AoC2015/Day06.java
+++ AoC2015/src/main/java/com/therifian/AoC2015/Day06.java
@@ -2,13 +2,13 @@ package com.therifian.AoC2015;
 
 public class Day06 {
 
-    private static final boolean[][] grid = new boolean[1000][1000];
+    private static final int[][] grid = new int[1000][1000];
 
-    public static int solve(String text) {
-        int on = 0, x1 = 0, y1 = 0 , x2 = 0, y2 = 0;
+    private static int gridManipulation(boolean part1, String input) {
+        int response = 0, x1 = 0, y1 = 0 , x2 = 0, y2 = 0;
         boolean turnOn = false, turnOff = false, toggle = false;
 
-        String[] s = text.split(" through ");
+        String[] s = input.split(" through ");
         String[] s2 = s[0].split(" ");
 
         if (s2.length == 3) {
@@ -34,15 +34,33 @@ public class Day06 {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid.length; j++) {
                 if (i >= x1 && i <= x2 && j >= y1 && j <= y2) {
-                    if (turnOn) grid[i][j] = true;
-                    if (turnOff) grid[i][j] = false;
-                    if (toggle) grid[i][j] = !grid[i][j];
-                }
-                if (grid[i][j]) on++;
+                    if (part1) {
+                        if (turnOn) grid[i][j] = 1;
+                        if (turnOff) grid[i][j] = 0;
+                        if (toggle) {
+                            grid[i][j] = grid[i][j] == 0 ? 1 : 0;
+                        }
+                    } else {
+                        if (turnOn) grid[i][j]++;
+                        if (turnOff) grid[i][j] -= grid[i][j] > 0 ?  1: 0;
+                        if (toggle) grid[i][j] += 2;
+                    }
 
+                }
+
+                if (part1) response += grid[i][j] == 1 ? 1 : 0;
+                else response += grid[i][j];
             }
         }
 
-        return on;
+        return response;
+    }
+
+    public static int part1(String input) {
+        return gridManipulation(true, input);
+    }
+
+    public static int part2(String input) {
+        return gridManipulation(false, input);
     }
 }
--- AoC2015/src/test/java/com/therifian/AoC2015/Day06Test.java
+++ AoC2015/src/test/java/com/therifian/AoC2015/Day06Test.java
@@ -8,16 +8,16 @@ public class Day06Test {
 
     @Test
     void test() {
-        assertEquals(1_000_000, Day06.solve("turn on 0,0 through 999,999"));
+        assertEquals(1_000_000, Day06.part1("turn on 0,0 through 999,999"));
     }
 
     @Test
     void test2() {
-        assertEquals(999_000, Day06.solve("toggle 0,0 through 999,0"));
+        assertEquals(999_000, Day06.part1("toggle 0,0 through 999,0"));
     }
 
     @Test
     void test3() {
-        assertEquals(998_996, Day06.solve("turn off 499,499 through 500,500"));
+        assertEquals(998_996, Day06.part1("turn off 499,499 through 500,500"));
     }
 }
