3048dce9dc904b9d381d383949680950d4d8ebe7
therifian <therifianeagle@gmail.com>
2025-08-23 00:10:32 +0200

    feat: part 1

--- AoC2015/src/main/java/com/therifian/AoC2015/Day12.java
+++ AoC2015/src/main/java/com/therifian/AoC2015/Day12.java
@@ -7,11 +7,25 @@ public class Day12 {
         char[] chars = input.toCharArray();
         for (int i = 0; i < chars.length; i++) {
             if (Character.isDigit(chars[i]) && i > 0 && chars[i-1] == '-') {
-                sum -= Character.getNumericValue(chars[i]);
+                int number = Day12.number(chars, i);
+                sum -= number;
+                i += String.valueOf(number).length();
             } else if (Character.isDigit(chars[i])) {
-                sum += Character.getNumericValue(chars[i]);
+                int number = Day12.number(chars, i);
+                sum += number;
+                i += String.valueOf(number).length();
             }
         }
         return sum;
+    }
+
+    private static int number(char[] input, int i) {
+        for (int j = i; j < input.length; j++) {
+            if  (!Character.isDigit(input[j])) {
+                String value = new String(input, i, j - i);
+                return Integer.parseInt(value);
+            }
+        }
+        return 0;
     }
 }
